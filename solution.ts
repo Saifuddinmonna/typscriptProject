f76e9fd486b28813b60bac9b8b36df5f5df9da3d
Saifuddinmonna <108563464+Saifuddinmonna@users.noreply.github.com>
2025-11-18 18:32:00 +0600

    problem 2 is solved

--- solution.ts
+++ solution.ts
@@ -1,4 +1,4 @@
-// Problem 1:
+
 function formatValue(value: string | number | boolean): string | number | boolean {
     if (typeof value === 'string') {
       return value.toUpperCase();
@@ -9,3 +9,13 @@ function formatValue(value: string | number | boolean): string | number | boolea
     }
     
     return value;}
+
+    function getLength(value: string | any[]): number {
+        if (typeof value === 'string') {
+          return value.length;
+        } else if (Array.isArray(value)) {
+          return value.length;
+        }
+       
+        return 0; 
+      }

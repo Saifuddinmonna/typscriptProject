75b353d38b2dd347cd0dcda43e3bf9eccea06a00
Saifuddinmonna <108563464+Saifuddinmonna@users.noreply.github.com>
2025-11-18 17:49:39 +0600

    problem 1 is solved

---
+++ solution.ts
@@ -0,0 +1,11 @@
+// Problem 1:
+function formatValue(value: string | number | boolean): string | number | boolean {
+    if (typeof value === 'string') {
+      return value.toUpperCase();
+    } else if (typeof value === 'number') {
+      return value * 10;
+    } else if (typeof value === 'boolean') {
+      return !value;
+    }
+    
+    return value;}
